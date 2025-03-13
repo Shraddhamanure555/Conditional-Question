d70b40521373d992f8b39923a036ce3a30d035a2
Shradha Manure <118459173+Shraddhamanure555@users.noreply.github.com>
2025-03-13 12:29:40 +0530

    VowelConsonants.java

--- VowelConsonants.java
+++ VowelConsonants.java
@@ -1,7 +1,20 @@
 //Vowel and Consonants 
 public class VowelConsonants {
 public static void main(String args[]) {
-	char a=
-	if
+	char voewls= 'a';
+	switch (voewls){
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case  'u':
+		System.out.println("Vowels");
+		break;
+		defualut:{
+			System.out.println("Invalid input..");
+			
+		}
+	}
 }
 }
+
